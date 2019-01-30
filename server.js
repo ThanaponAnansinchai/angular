f36e8a63190827c7248c4a03151b409877655a23
Tanaphon <thanapon.anan@gmail.com>
2019-01-30 14:55:03 +0700

    Edit http request

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const app = express();
 
 app.use(express.static('./dist/abc'));
 app.use(cors());
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname,'/dist/abc/index.html'));
+app.get('/*', function (req, res) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.sendFile(path.join(__dirname, '/dist/abc/index.html'));
 
 });
 // Start the app by listening on the default Heroku port
