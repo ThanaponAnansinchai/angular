03c56e0cc7a8afdb64d767cda92e10a11823d78e
Tanaphon <thanapon.anan@gmail.com>
2019-01-30 14:42:00 +0700

    Edit http request

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+
+
 
 // Serve only the static files form the dist directory
 
 app.use(express.static('./dist/abc'));
+app.use(cors());
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname,'/dist/abc/index.html'));
 
