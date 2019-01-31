5fe04dab7116e16905ea7aa7d35ae4155be28f39
Tanaphon <thanapon.anan@gmail.com>
2019-01-31 10:17:27 +0700

    Modified server api

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 // Serve only the static files form the dist directory
-
+/*
 app.use(function (req, res, next){
     if (req.headers['x-forwarded-proto'] === 'https') {
       res.redirect('http://' + req.hostname + req.url);
@@ -15,6 +15,18 @@ app.use(function (req, res, next){
       next();
     }
   });
+*/
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  app.use(forceSSL());
 
 app.use(express.static('./dist/abc'));
 
