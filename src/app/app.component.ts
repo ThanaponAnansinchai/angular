a10302b00cf3f84674c162195f20145a8cc30674
Tanaphon <thanapon.anan@gmail.com>
2019-02-05 10:39:02 +0700

    Redesing UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Connection, DataService } from './connected.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
   public present;
   public rooms: any[] = [];
@@ -14,8 +17,12 @@ export class AppComponent {
   public Etime: string[][] = [];
   public status: any[] = [];
   public time_status: any[] = [];
+  public start_period: any[]=[];
+  public end_period: any[]=[];
   public searchText: string;
   public status_color: any;
+  public am = "AM";
+  public pm = "PM";
 
   constructor(private apiService: Connection, private route: Router, public dataService: DataService) {}
   
@@ -38,6 +45,10 @@ export class AppComponent {
         this.Stime.push((room.time.start_time).slice(0, -8).split("T"));
         this.Etime.push((room.time.end_time).slice(0, -8).split("T"));
         this.initializeClock(index, room.time.start_time, room.time.end_time);
+        let start = this.getTimeremaining(room.time.start_time);
+        let end = this.getTimeremaining(room.time.end_time);
+        this.start_period.push(start.period);
+        this.end_period.push(end.period);
         index++;
       });
     });
@@ -47,6 +58,7 @@ export class AppComponent {
     this.route.navigate(['v2/room/' + id]);
   }
 
+ 
   initializeClock(index, startTime, endTime) {
     this.updateClock(index, startTime, endTime);
     let timeInterval = setInterval(() => {
@@ -64,7 +76,7 @@ export class AppComponent {
         this.status_color = "#ecd31f"
       }
       else if (start.total <= 0) {
-        this.status[index] = 'Live ';
+        this.status[index] = 'กำลังทำการถ่ายทอดสด ';
         this.time_status[index] = end.hour + " ชั่วโมง " + end.minute + " นาทีจะสิ้นสุดการถ่ายทอดสด";
         this.status_color = "#5cb85c"
       }
@@ -82,15 +94,24 @@ export class AppComponent {
     let total = Date.parse(time) - Date.parse(this.present);
     let minute = Math.floor((total / 1000 / 60) % 60);
     let hour = Math.floor((total / (1000 * 60 * 60)));
-    return {
-      'total': total, 'hour': hour, 'minute': minute
-    };
+    if(hour < 12){
+      return {
+        'total': total, 'hour': hour, 'minute': minute, 'period':this.am
+      };
+    }
+    else{
+        hour = hour % 12;
+        return {
+          'total': total, 'hour': hour, 'minute': minute,'period':this.pm
+        };
+    }
+    
   }
   updateClock(index, startTime, endTime) {
     let start = this.getTimeremaining(startTime);
     let end = this.getTimeremaining(endTime);
     if (end.total <= 0) {
-      this.status[index] = "จบการถ่ายทอดสดแล้ว";
+      this.status[index] = "การถ่ายทอดสดสิ้นสุดแล้ว";
       this.time_status[index] = '';
       this.status_color = "#cccccc"
     }
@@ -111,12 +132,14 @@ export class AppComponent {
     }
   }
 
+
   w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
   }
 
   w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
   }
-
 }
--- src/app/components/show/show.component.ts
+++ src/app/components/show/show.component.ts
@@ -2,23 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { DataService ,Connection} from '../../connected.service';
 
+
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
 export class ShowComponent implements OnInit {
-  public id: number;
-  public sub: any;
-  public data;
-  public title = '';
-  public description = '';
-  public start_time;
-  public end_time;
-  public connectivity;
+  id: number;
+  private sub: any;
+  data;
+  title = '';
+  description = '';
+  start_time;
+  end_time;
+  connectivity;
+  present;
+  start_period;
+  end_period;
+  public am = "AM";
+  public pm = "PM";
   public expanded :boolean = false;
-  
-  constructor(private route: ActivatedRoute,public dataService: DataService,private apiService:Connection) { this.expanded = this.dataService.hide;}
+  constructor(private route: ActivatedRoute,public dataService: DataService,private apiService:Connection) { }
   
   ngOnInit() {
    
@@ -28,6 +33,7 @@ export class ShowComponent implements OnInit {
       this.apiService.getData(this.id).subscribe((data: any[]) => {
         let a = JSON.stringify(data);
         let b = JSON.parse(a);
+        
         this.data = b.result;
         this.title = this.data.title;
         this.connectivity = this.data.connectivity;   
@@ -38,6 +44,10 @@ export class ShowComponent implements OnInit {
           if(element.id === this.id){
             this.start_time = (element.time.start_time).slice(0, -8).split("T");
             this.end_time = (element.time.end_time).slice(0, -8).split("T");
+            let start = this.getTimeremaining(this.start_time);
+            let end = this.getTimeremaining(this.end_time);
+            this.start_period = start.period;
+            this.end_period = end.period;
           }          
         });
       });
@@ -45,4 +55,21 @@ export class ShowComponent implements OnInit {
 
   }
 
+  getTimeremaining(time) {
+    this.present = new Date();
+    let total = Date.parse(time) - Date.parse(this.present);
+    let minute = Math.floor((total / 1000 / 60) % 60);
+    let hour = Math.floor((total / (1000 * 60 * 60)));
+    if(hour < 12){
+      return {
+        'total': total, 'hour': hour, 'minute': minute, 'period':this.am
+      };
+    }
+    else{
+        hour = hour % 12;
+        return {
+          'total': total, 'hour': hour, 'minute': minute,'period':this.pm
+        };
+    }
+  }
 }
