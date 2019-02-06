f06c32c704c60a076fc6b14afdc3986f7354f88d
Tanaphon <thanapon.anan@gmail.com>
2019-02-06 10:53:28 +0700

    Add route to get url

--- server.js
+++ server.js
@@ -13,12 +13,18 @@ app.use(function (req, res, next){
     }
   });
 
-app.route('/*').get(function(req,res) {
-  return res.sendFile(path.join(config.root, 'index.html'))
-})
+
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/abc'));
+
+app.get('/*', function (req, res, next) {
+  if (/.js|.html|.css|templates|js|scripts/.test(req.path) || req.xhr) {
+  return next({ status: 404, message: 'Not Found' });
+  } else {
+     return res.render('index');
+  }
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
