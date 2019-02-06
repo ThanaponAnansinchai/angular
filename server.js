04e9f8948143242389a32178a5afb346d6752714
Tanaphon <thanapon.anan@gmail.com>
2019-02-06 11:36:42 +0700

    Add route to get url

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static('./dist/abc'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, './src/index.html'), function(err) {
     if (err) {
-      res.status(500).send(err)
+      res.status(500).send("ABCDEFG")
     }
   })
 })
