4dd7b0b084a1dd79f092c325bd5eefaa90c2db16
Tanaphon <thanapon.anan@gmail.com>
2019-03-21 09:18:45 +0700

    New option setting

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Connection , optionService } from '../../service/connected.service';
+import { Connection, optionService } from '../../service/connected.service';
 
 
 @Component({
@@ -9,11 +9,11 @@ import { Connection , optionService } from '../../service/connected.service';
   styleUrls: ['./options.component.css']
 })
 export class OptionsComponent implements OnInit {
- 
+
   public isMobile = false;
   public atextSize = [10, 16, 18, 22, 24, 26, 28, 36, 48, 72, 84, 96, 100];
   public size = 36;
-  public fontColorPicker; widths; fontsize; fontStyle;  color; bgColor; captionBG;
+  public fontColorPicker; widths; fontsize; fontStyle; color; bgColor; captionBG;
   public font = "FreesiaUPC";
   public mode = "video-text"
   public CaptionedWidth = "70";
@@ -23,16 +23,16 @@ export class OptionsComponent implements OnInit {
   public captioned_default = "captioned_default";
   private temp: any[] = [];
   public options; optionVideoMode; myModal; m_options; m_optionVideoMode; choice;
-  public statusStreaming; terminateViewer; 
+  public statusStreaming; terminateViewer;
   public settingCaptionedLine; settingCaptionedWidth;
   public dropupContent = "hide"
 
   // Fullscreen
 
-  public exit_fullscreen; incFont2; decFont2; inputFont2; viewerMode2; full; 
+  public exit_fullscreen; incFont2; decFont2; inputFont2; viewerMode2; full;
   public fullscreenHeight = "normal-height";
   public isFullscreen = false;
-  
+
 
   constructor(private _route: Router, private route: ActivatedRoute, private apiService: Connection, public optionService: optionService) { }
 
@@ -48,22 +48,22 @@ export class OptionsComponent implements OnInit {
       this.isMobile = false;
     }
 
-    this.optionService.fontColorPicker = this.fontColorPicker; 
-    this.optionService.widths = this.widths; 
-    this.optionService.fontsize =  this.fontsize; 
-    this.optionService.fontStyle =  this.fontStyle;  
-    this.optionService.color = this.color; 
-    this.optionService.bgColor = this.bgColor; 
+    this.optionService.fontColorPicker = this.fontColorPicker;
+    this.optionService.widths = this.widths;
+    this.optionService.fontsize = this.fontsize;
+    this.optionService.fontStyle = this.fontStyle;
+    this.optionService.color = this.color;
+    this.optionService.bgColor = this.bgColor;
     this.optionService.captionBG = this.captionBG;
-  
-    
-    this.m_options = this.m_optionVideoMode = this.myModal =  "hide";
+
+
+    this.m_options = this.m_optionVideoMode = this.myModal = "hide";
     this.exit_fullscreen = this.incFont2 = this.decFont2 = this.inputFont2 = this.viewerMode2 = this.choice = "hide"
 
   }
 
 
-   // Viewer section //
+  // Viewer section //
 
   toIntPlus(size, change) {
     this.optionService.size = this.size = parseInt(size) + parseInt(change)
@@ -79,41 +79,41 @@ export class OptionsComponent implements OnInit {
   }
 
   setFont(fontStyle) {
-    
-    
+
+
     this.optionService.fontStyle = this.fontStyle = fontStyle;
   }
 
   setMode(mode) {
-  
-    
+
+
     if (mode == "text-only") {
-
-      this.settingCaptionedLine = this.settingCaptionedWidth = "hide";
-
-
-      this.temp[0] = this.classCaptionedWidth;
-      this.temp[1] = this.classCaptionedLine;
-      this.optionService.classCaptionedWidth =  this.classCaptionedWidth = "captioned-text-only";
-      this.optionService.classCaptionedLine = this.classCaptionedLine = "";
-      this.optionService.videoTextOnly = "show-inline";
-      this.optionService.video = "hide";
+      if (this.mode != "text-only") {
+        this.settingCaptionedLine = this.settingCaptionedWidth = "hide";
+        this.temp[0] = this.classCaptionedWidth;
+        this.temp[1] = this.classCaptionedLine;
+        this.optionService.classCaptionedWidth = this.classCaptionedWidth = "captioned-text-only";
+        this.optionService.classCaptionedLine = this.classCaptionedLine = "";
+        this.optionService.videoTextOnly = "show-inline";
+        this.optionService.video = "hide";
+       
+      }
       this.mode = "text-only"
 
     }
     if (mode == "video-text") {
 
-      if(this.mode != "video-text"){
+      if (this.mode != "video-text") {
         this.optionService.classCaptionedWidth = this.classCaptionedWidth = this.temp[0];
-        this.optionService.classCaptionedLine =this.classCaptionedLine = this.temp[1];
+        this.optionService.classCaptionedLine = this.classCaptionedLine = this.temp[1];
         this.settingCaptionedLine = this.settingCaptionedWidth = "inline";
         this.optionService.videoTextOnly = "hide";
         this.optionService.video = "show-inline";
-        this.mode = "video-text"
+       
       }
-      
 
-     
+      this.mode = "video-text"
+
     }
   }
 
@@ -137,7 +137,7 @@ export class OptionsComponent implements OnInit {
       this.optionService.classCaptionedLine = this.classCaptionedLine = "captioned-line-2"
     }
   }
-  
+
   openModal() {
     //this.myModal = "show-block"
     this.dropupContent = "show-block"
@@ -147,11 +147,11 @@ export class OptionsComponent implements OnInit {
     this.myModal = "hide"
   }
 
-  toggleOption(){
-    if(this.optionService.dropupContent == "hide"){
+  toggleOption() {
+    if (this.optionService.dropupContent == "hide") {
       this.optionService.dropupContent = "show-block"
     }
-    else{
+    else {
       this.optionService.dropupContent = "hide"
     }
   }
