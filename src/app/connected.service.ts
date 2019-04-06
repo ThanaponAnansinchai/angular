4cea22876b8a6b1ff7c5661bb397d289fa328bb7
Tanaphon <thanapon.anan@gmail.com>
2019-04-06 09:36:54 +0700

    New Option setting

---
+++ src/app/connected.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Connection {
+    
+  API_URL  =  'http://api.debug.rtt.in.th/v2/rooms';
+  test_url = 'http://api.rtt.in.th';
+  test_port = ':12119'
+
+  constructor(private  httpClient:  HttpClient) {}
+
+  getAllData(){
+    return  this.httpClient.get(`${this.API_URL}`)
+}
+  getData(id){
+    return  this.httpClient.get(`${this.API_URL}/`+id)
+}
+  
+  testapi(){
+    return this.httpClient.get(`${this.test_url}`+this.test_port)
+  }
+}
+
+@Injectable() 
+export class DataService {
+  serviceData :any[] = []; 
+  hide: boolean = false;
+}
